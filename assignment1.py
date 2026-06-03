8c4883f19b534aeb4e1dcc76506529997f10f615
DataSpectorX <anurag042533@gmail.com>
2026-06-03 11:00:35 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,57 @@
+#Question 1
+#int
+a=24
+print(a)
+print(type(a))
+#float
+height = 5.8
+print(height)
+print(type(height))
+#
+name = "Gargi"
+print(name)
+print(type(name))
+#
+is_student = True
+print(is_student)
+print(type(is_student))
+#Question 2
+name = "Gargi"
+age = 20
+city = "Jhunjhunu"
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+# Question
+name = input("Enter your name: ")
+
+print("Name in Uppercase:", name.upper())
+print("Total Characters:", len(name))
+#Question 4 in Readme file
+# upper case
+name = "gargi"
+print(name.upper())
+# lower case
+name = "GARGI"
+print(name.lower())
+#capitalized
+name= "gargi"
+print(name.capitalize())
+#replace
+text = "I like Python"
+print(text.replace("Python", "Coding"))
+#find
+text = "I love Python"
+position = text.find("Python")
+print(position)
+#Question 5
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+print("Complete List:", fruits)
+print("First:", fruits[0])
+print("Last:", fruits[-1])
+print("Total:", len(fruits))
+#Question 6
+numbers = [10, 20, 30, 40, 50]
+numbers.append(60)
+numbers.remove(20)
+print("Updated List:", numbers)
